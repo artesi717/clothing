55ceddd94bc9ff427568588b48999b72dfb305bf
artesi717 <artesberisha0981@gmail.com>
2021-09-02 14:27:44 +0200

    First commit frontend

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,510 @@
+<div class="carouseli1">
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item img-fluid active">
+                <img class="d-block w-100 foto img-fluid" src="https://underarmourme.com/storage/app/media/Campaigns/ua-sportsmask/video-cover.jpg" alt="First slide">
+            </div>
+            <div class="carousel-item img-fluid">
+                <img class="d-block w-100 foto img-fluid" src="https://s3.us-east-1.amazonaws.com/vnda-cockpit/www-streetopia-me/2021/03/23/605a5d80b21a1fullbanner_1920x800px.jpg" alt="Second slide">
+            </div>
+            <div class="carousel-item img-fluid">
+                <img class="d-block w-100 foto img-fluid" src="https://i8.amplience.net/i/jpl/desktop-homepage-top-banner-1920x840-b32e6f9f3a23eae14d4bf97d2e981991?qlt=80&fmt=webp" alt="Third slide">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+</div>
+
+<br>
+
+<section id="cliens" class="cliens section-bg" data-aos="fade-up">
+    <div class="container">
+
+        <div class="row" data-aos="zoom-in">
+            <div class="col-lg-2 col-md-4 col-8 d-flex align-items-center justify-content-center">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/3/36/Logo_nike_principal.jpg" class="img-fluid" alt="">
+            </div>
+
+            <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Adidas_Logo.svg/1024px-Adidas_Logo.svg.png" class="img-fluid" alt="">
+            </div>
+
+            <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                <img src="https://cdn.shopify.com/s/files/1/2374/3709/products/The_north_face_thumb_8fd74e2f-763e-42dd-ace2-4f0fa4180527_1080x.jpg" class="img-fluid" alt="">
+            </div>
+
+            <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Under_armour_logo.svg/2560px-Under_armour_logo.svg.png" class="img-fluid" alt="">
+            </div>
+
+            <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                <img src="https://logos-world.net/wp-content/uploads/2020/04/Air-Jordan-Symbol.png" class="img-fluid" alt="">
+            </div>
+
+            <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                <img src="https://i.pinimg.com/originals/7b/91/5e/7b915e395a3880edc9a0cf12ac13cd96.jpg" class="img-fluid" alt="">
+            </div>
+        </div>
+
+    </div>
+</section>
+
+
+<br><br>
+<h1 class="shopby">Shop By Category</h1>
+
+<br> <br>
+
+<div class="row13" data-aos="fade-left">
+    <div class="column1">
+        <div class="card3">
+            <a href=""> <img width="91%" src="https://media.kohlsimg.com/is/image/kohls/3593715_Baroque_Green?wid=600&hei=600&op_sharpen=1" alt=""> </a><br>
+            <a class="shop11" href="">Shop Clothing</a>
+        </div>
+    </div>
+
+    <div class="column1">
+        <div class="card3">
+            <a href=""> <img width="80%" src="https://i.ibb.co/V0F4JFG/output-onlinepngtools-10.png" alt=""></a> <br>
+            <a class="shop11" href="">Shop Footwear</a>
+        </div>
+    </div>
+
+    <div class="column1">
+        <div class="card3">
+            <a href=""> <img width="80%" src="https://res.cloudinary.com/www-flook-co-za/image/upload/c_fit,q_auto:eco/v1/product-image/128945.jpg" alt=""></a> <br>
+            <a class="shop11" href="">Shop Accessories</a>
+        </div>
+    </div>
+
+</div>
+
+
+
+
+<br><br>
+
+
+
+
+<img data-aos="fade-down" width="100%" src="https://i8.amplience.net/i/jpl/desktop-1680x244-eb12ebfebf34cd60ef04bc792183862f?qlt=80&fmt=webp" alt="">
+<br>
+<h1 class="latestdr">Latest Drops</h1>
+<div class="main" data-aos="fade-right">
+    <ul class="cards">
+        <li class="cards_item">
+            <div class="card">
+                <div class="card_image"><img class="cardsfoto" src="https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/cj2jbeomutdbn0tcrbza/air-max-97-shoes-nsRddC.png"></div>
+                <div class="card_content">
+                    <h1 class="card_title1">170€</h1> <br>
+                    <!--  <p class="card_text">Size : 40-45 <br> Available colors : Black , red , green</p> -->
+                    <h2 class="card_title">Nike Air Max 97</h2> <br>
+                    <button class="btn card_btn">Buy now</button>
+                </div>
+            </div>
+        </li>
+        <li class="cards_item">
+            <div class="card">
+                <div class="card_image"><img class="cardsfoto" src="https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/ebad848a-13b1-46d5-a85e-49b4b6a4953c/air-force-1-le-older-shoe-cJV32q.png"></div>
+                <div class="card_content">
+                    <h1 class="card_title1">97€</h1> <br>
+                    <!--  <p class="card_text">Size : 40-45 <br> Available colors : Black , red , green</p> -->
+                    <h2 class="card_title">Nike Air Force 1</h2> <br>
+                    <button class="btn card_btn">Buy now</button>
+                </div>
+            </div>
+        </li>
+        <li class="cards_item">
+            <div class="card">
+                <div class="card_image"><img class="cardsfoto" src="https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/463c4fbf-d4a7-420b-af4b-3d563ae3ed7b/sportswear-tech-fleece-hoodie-Rd23Gq.png"></div>
+                <div class="card_content">
+                    <h1 class="card_title1">106€</h1> <br>
+                    <!--  <p class="card_text">Size : 40-45 <br> Available colors : Black , red , green</p> -->
+                    <h2 class="card_title">Nike Sportswear Tech Fleece</h2> <br>
+                    <button class="btn card_btn">Buy now</button>
+                </div>
+            </div>
+        </li>
+        <li class="cards_item">
+            <div class="card">
+                <div class="card_image"><img class="cardsfoto" src="https://underarmour.scene7.com/is/image/Underarmour/1342652-001_SLF_SL?rp=standard-0pad|pdpMainDesktop&scl=1&fmt=jpg&qlt=85&resMode=sharp2&cache=on,on&bgc=F0F0F0&wid=566&hei=708&size=566,708"></div>
+                <div class="card_content">
+                    <h1 class="card_title1">16€</h1> <br>
+                    <!--  <p class="card_text">Size : 40-45 <br> Available colors : Black , red , green</p> -->
+                    <h2 class="card_title">Youth UA Scrimmage 2.0 Backpack</h2> <br>
+                    <!-- <button class="btn card_btn">Buy now</button> -->
+                </div>
+            </div>
+        </li>
+        <li class="cards_item">
+            <div class="card">
+                <div class="card_image"><img class="cardsfoto" src="https://i8.amplience.net/i/jpl/jd_390387_b?qlt=92&w=600&h=765&v=1"></div>
+                <div class="card_content">
+                    <h1 class="card_title1">28€</h1> <br>
+                    <!--  <p class="card_text">Size : 40-45 <br> Available colors : Black , red , green</p> -->
+                    <h2 class="card_title">The North Face Easy T-shirt</h2> <br>
+                    <!-- <button class="btn card_btn">Buy now</button> -->
+                </div>
+            </div>
+        </li>
+        <li class="cards_item">
+            <div class="card">
+                <div class="card_image"><img class="cardsfoto" src="https://ridestore.imgix.net/catalog/product/E/5/E5862_1.jpg"></div>
+                <div class="card_content">
+                    <h1 class="card_title1">62€</h1> <br>
+                    <!--  <p class="card_text">Size : 40-45 <br> Available colors : Black , red , green</p> -->
+                    <h2 class="card_title">TBlue Logo Regular Fit BG Trefoil Sports Jacket</h2> <br>
+                    <!-- <button class="btn card_btn">Buy now</button> -->
+                </div>
+            </div>
+        </li>
+    </ul>
+</div>
+
+<br><br>
+
+
+
+
+<style>
+    .shop11 {
+        float: center;
+        font-size: 26px;
+        color: black;
+        font-family: 'DM Sans', sans-serif;
+        font-weight: 800;
+    }
+
+    /* Float four columns side by side */
+    .column1 {
+        float: left;
+        width: 25%;
+    }
+
+    /* Remove extra left and right margins, due to padding */
+    .row13 {
+        margin: 0 -5px;
+        display: flex;
+        justify-content: center;
+    }
+
+    /* Clear floats after the columns */
+    .row13:after {
+        content: "";
+        display: table !important;
+        clear: both;
+    }
+
+    /* Responsive columns */
+    @media screen and (max-width: 600px) {
+        .column1 {
+            width: 100%;
+            display: block;
+            margin-bottom: 20px;
+        }
+    }
+
+    /* Style the counter cards */
+    .card3 {
+        padding: 16px;
+        text-align: center;
+        background-color: white;
+    }
+
+
+    .latestdr {
+        font-family: 'DotGothic16', sans-serif;
+        font-weight: 800;
+        padding-top: 50px;
+        text-align: center;
+    }
+
+    .shopby {
+        margin-left: 20%;
+        font-family: 'DM Sans', sans-serif;
+        font-weight: 800;
+    }
+
+    body {
+        padding-top: 37px;
+    }
+
+    .carouseli1 {
+        height: 800px;
+    }
+
+    .foto {
+        height: 800px;
+        filter: brightness(52%);
+        width: 100% \50;
+
+    }
+
+    .carousel .carousel-item {
+        opacity: 0;
+        transition-duration: .6s;
+        transition-property: opacity;
+    }
+
+    .carousel .carousel-item.active,
+    .carousel .carousel-item-next.carousel-item-left,
+    .carousel .carousel-item-prev.carousel-item-right {
+        opacity: 1;
+    }
+
+    .carousel .active.carousel-item-left,
+    .carousel .active.carousel-item-right {
+        opacity: 0;
+    }
+
+    .carousel .carousel-item-next,
+    .carousel .carousel-item-prev,
+    .carousel .carousel-item.active,
+    .carousel .active.carousel-item-left,
+    .carousel .active.carousel-item-prev {
+        transform: translateX(0);
+        transform: translate3d(0, 0, 0);
+    }
+
+
+
+    /* Font */
+    @import url('https://fonts.googleapis.com/css?family=Quicksand:400,700');
+
+    /* Design */
+    *,
+    *::before,
+    *::after {
+        box-sizing: border-box;
+    }
+
+    html {
+        background-color: #ecf9ff;
+    }
+
+    body {
+        color: #272727;
+        font-family: 'DM Sans', sans-serif;
+        font-style: normal;
+        font-weight: 400;
+        letter-spacing: 0;
+    }
+
+    .main {
+        max-width: 1200px;
+        margin: 0 auto;
+    }
+
+
+    .cardsfoto {
+        height: auto;
+        max-width: 70%;
+        max-height: 100%;
+        vertical-align: middle;
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        object-fit: cover;
+    }
+
+    .btn {
+        color: #ffffff;
+        padding: 0.8rem;
+        font-size: 14px;
+        text-transform: uppercase;
+        border-radius: 4px;
+        font-weight: 400;
+        display: block;
+        width: 100%;
+        cursor: pointer;
+        border: 1px solid rgba(255, 255, 255, 0.2);
+        background: transparent;
+    }
+
+    .btn:hover {
+        background-color: rgba(255, 255, 255, 0.12);
+    }
+
+    .cards {
+        display: flex;
+        flex-wrap: wrap;
+        list-style: none;
+        margin: 0;
+        padding: 0;
+    }
+
+    .cards_item {
+        display: flex;
+        padding: 1rem;
+    }
+
+    @media (min-width: 40rem) {
+        .cards_item {
+            width: 50%;
+        }
+    }
+
+    @media (min-width: 56rem) {
+        .cards_item {
+            width: 33.3333%;
+        }
+    }
+
+    .card {
+        background-color: white;
+        border-radius: 0.25rem;
+        box-shadow: 0 20px 40px -14px rgba(0, 0, 0, 0.25);
+        display: flex;
+        flex-direction: column;
+        overflow: hidden;
+
+    }
+
+    .card_content {
+        padding: 1rem;
+        background-color: #457B9D;
+    }
+
+    .card_title {
+        color: #ffffff;
+        font-size: 1.1rem;
+        font-weight: 400;
+        letter-spacing: 1px;
+        text-transform: capitalize;
+        margin: 0px;
+    }
+
+    .card_title1 {
+        color: #ffffff;
+        font-size: 1.5rem;
+        font-weight: 800;
+        letter-spacing: 1px;
+        text-transform: capitalize;
+        margin: 0px;
+    }
+
+    .card_text {
+        color: #ffffff;
+        font-size: 0.875rem;
+        line-height: 1.5;
+        margin-bottom: 1.25rem;
+        font-weight: 400;
+    }
+
+    .made_by {
+        font-weight: 400;
+        font-size: 13px;
+        margin-top: 35px;
+        text-align: center;
+    }
+
+
+
+    section {
+        padding: 60px 0;
+        overflow: hidden;
+    }
+
+    .section-bg {}
+
+    .section-title {
+        text-align: center;
+        padding-bottom: 30px;
+    }
+
+    .section-title h2 {
+        font-size: 32px;
+        font-weight: bold;
+        text-transform: uppercase;
+        margin-bottom: 20px;
+        padding-bottom: 20px;
+        position: relative;
+        color: #37517e;
+    }
+
+    .section-title h2::before {
+        content: '';
+        position: absolute;
+        display: block;
+        width: 120px;
+        height: 1px;
+        background: #ddd;
+        bottom: 1px;
+        left: calc(50% - 60px);
+    }
+
+    .section-title h2::after {
+        content: '';
+        position: absolute;
+        display: block;
+        width: 40px;
+        height: 3px;
+        background: #47b2e4;
+        bottom: 0;
+        left: calc(50% - 20px);
+    }
+
+    .section-title p {
+        margin-bottom: 0;
+    }
+
+    /*--------------------------------------------------------------
+# Cliens
+--------------------------------------------------------------*/
+    .cliens {
+        padding: 12px 0;
+        text-align: center;
+    }
+
+    .cliens img {
+        max-width: 45%;
+        transition: all 0.4s ease-in-out;
+        display: inline-block;
+        padding: 15px 0;
+        -webkit-filter: grayscale(100);
+        filter: grayscale(100);
+    }
+
+    .cliens img:hover {
+        -webkit-filter: none;
+        filter: none;
+        transform: scale(1.1);
+    }
+
+    @media (max-width: 768px) {
+        .cliens img {
+            max-width: 40%;
+        }
+    }
+</style>
+
+<script>
+    $('.carousel').carousel({
+        interval: 6000,
+        pause: "false"
+    });
+
+    AOS.init({
+        duration: 1500
+
+    });
+    AOS.init();
+    AOS.init({
+        once: true
+    })
+
+
+    // AOS.init({
+    //    offset: 200,
+    //    duration: 600,
+    //    easing: 'ease-in-sine',
+    //    delay: 100,
+    //  });
+</script>
---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,243 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+    <!-- fontawesome -->
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+    <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+
+    <!-- regular fonts -->
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <!-- font -->
+
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=DotGothic16&display=swap" rel="stylesheet">
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+    <link rel="stylesheet" href="bower_components/aos/dist/aos.css">
+    <script src="bower_components/aos/dist/aos.js"></script>
+    <link href="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.css" rel="stylesheet">
+    <script src="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.js"></script>
+
+
+    <title>Clothing</title>
+</head>
+
+<body>
+
+    <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#"><img width="115px" src="https://www.seekpng.com/png/full/70-705495_15-jordan-png-for-free-download-on-mbtskoudsalg.png" alt=""></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <!--  <li class="nav-item active">
+                    <a style="color: white;" class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li> -->
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Men
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Women
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Kids
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Accessories
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Footwear
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li>
+
+                <li class="nav-item shopingcart dropdown">
+                    <a class="nav-link" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i style="font-size:26px;" class="fas fa-shopping-cart"></i>
+                    </a>
+                </li>
+
+
+
+
+
+            </ul> <br>
+            <form class="form-inline my-2 my-lg-0">
+                <div class="search-box">
+                    <input class="search-input" type="text" name="" placeholder="Search">
+                    <a href="#" class="search-btn">
+                        <i class="fas fa-search"></i>
+                    </a>
+                </div>
+            </form>
+
+
+        </div>
+    </nav>
+
+    <br><br>
+
+</body>
+
+<style>
+    .carta121 {
+        width: auto;
+    }
+
+    .navbar {
+        background: rgb(29, 53, 87);
+        background: linear-gradient(90deg, rgba(29, 53, 87, 1) 0%, rgba(107, 168, 255, 1) 58%, rgba(181, 202, 232, 1) 100%);
+    }
+
+    .shopingcart {
+        margin-left: 700px;
+    }
+
+    .navbar-light .navbar-nav .nav-link {
+        color: white;
+        float: center;
+        font-family: 'DM Sans', sans-serif;
+        font-weight: 800;
+        padding: 5px;
+        margin-left: 30px;
+        margin-right: 30px;
+    }
+
+    .navbar-light .navbar-nav .nav-link:hover {
+        color: #CACFD2;
+        float: center;
+        font-size: 17.5px;
+        font-family: 'DM Sans', sans-serif;
+        font-weight: 800;
+    }
+
+    .navbar-light .navbar-nav .nav-link:focus {
+        color: #CACFD2;
+    }
+
+
+    .btn {
+        padding: 5px 10px;
+        color: white;
+        border: 1px solid white;
+    }
+
+    .btn:hover {
+        color: black;
+        background-color: #F1FAEE;
+    }
+
+
+
+    .search-box {
+        position: absolute;
+        top: 50%;
+        left: 75%;
+        transform: translate(-50%, -50%);
+        background: #F1FAEE;
+        height: 70px;
+        border-radius: 50px;
+        padding: 10px;
+    }
+
+    .search-btn {
+        color: white;
+        float: right;
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+        background: #F1FAEE;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        transition: 2s;
+    }
+
+    .fa-search {
+        background: white;
+        font-size: 24px;
+        color: #1D3557;
+    }
+
+    .search-input {
+        border: none;
+        background: none;
+        outline: none;
+        float: left;
+        padding: 0;
+        color: black;
+        font-size: 20px;
+        transition: 0.4s;
+        line-height: 50px;
+        width: 0;
+    }
+
+    .search-box:hover>.search-input {
+        width: 200px;
+        padding: 0 7px;
+    }
+</style>
+
+
+</html>
